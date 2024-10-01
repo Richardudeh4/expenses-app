fd6321129079260fa2e19df8df8b97365528e613
richardudeh4 <richardudeh03@gmail.com>
2024-10-01 19:02:45 +0100

    setting up navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ManageExpenses from './screens/ManageExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview(){
+  return(
+  <BottomTabs.Navigator>
+    <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses}/>
+    <BottomTabs.Screen name="AllExpenses" component={AllExpenses}/>
+  </BottomTabs.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName='ExpensesOverview'>
+        <Stack.Screen name='ManageExpense' component={ManageExpenses}/>
+        <Stack.Screen name="ExpensesOverview" component={ExpensesOverview}/>
+      </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 

2b8d9488e118cae78c481fb6939b671f73f9ff33
richardudeh4 <richardudeh03@gmail.com>
2024-10-03 17:24:24 +0100

    added dummy data to the app

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ExpensesSummary from '../components/ExpensesOutput/ExpensesSummary'
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
 
 const AllExpenses = () => {
   return (
-    <View>
-      <Text>AllExpenses</Text>
-    </View>
+  <ExpensesOutput  ExpensesPeriod="Total"/>
   )
 }
 
